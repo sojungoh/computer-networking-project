c796f1e3d51120bbbf59b59ccea6884ddc061db3
2020042879 <sojung3514@gmail.com>
2023-05-18 22:56:15 +0900

    UDPServer class 구현

--- simple-udp/udp/src/UDPServer.java
+++ simple-udp/udp/src/UDPServer.java
@@ -1,11 +1,31 @@
-import java.io.*;
 import java.net.*;
 
 public class UDPServer {
-    public static void main(String args[]) throws Exception {
-        DatagramSocket serverSocket = new DatagramSocket();
+    static final String serverName = "hostname";
+    static final int serverPort = 12000;
 
-        
+    public static void main(String args[]) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(serverPort);
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+
+        try {
+
+            while(true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                String sentence = new String(receivePacket.getData());
+                InetAddress IPAddress = receivePacket.getAddress();
+                int port = receivePacket.getPort();
+                String modifiedSentence = sentence.toUpperCase();
+                sendData = modifiedSentence.getBytes();
+
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                serverSocket.send(sendPacket);
+            }   
+        } catch (Exception e) {
+            serverSocket.close();
+        }
     }
 
     
